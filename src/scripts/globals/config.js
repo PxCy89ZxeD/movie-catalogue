9d9fabbf43fcdaaf8ea0da914ea3345f8db3620d
unknown <ichsanmf14@gmail.com>
2023-11-28 20:35:39 +0800

    Add dotenv and reconfigure webpack and jest

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  KEY: 'YOUR_API_KEY',
+  KEY: '19f3d8759cde2a464d7ba7e82267c4d3',
   BASE_URL: 'https://api.themoviedb.org/3/',
   BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
   DEFAULT_LANGUAGE: 'en-us',
@@ -10,12 +10,9 @@ const CONFIG = {
 
   WEB_SOCKET_SERVER: 'wss://stormy-badlands-06326.herokuapp.com',
 
-  PUSH_MSG_VAPID_PUBLIC_KEY:
-    'BN7-r0Svv7CsTi18-OPYtJLVW0bfuZ1x1UtrygczKjennA_qs7OWmgOewcuYSYF3Gc_mPbqsDh2YoGCDPL0RxDQ',
-  PUSH_MSG_SUBSCRIBE_URL:
-    'https://dicoding-movie-push-notif.netlify.app/.netlify/functions/subscribe',
-  PUSH_MSG_UNSUBSCRIBE_URL:
-    'https://dicoding-movie-push-notif.netlify.app/.netlify/functions/unsubscribe',
+  PUSH_MSG_VAPID_PUBLIC_KEY: 'BN7-r0Svv7CsTi18-OPYtJLVW0bfuZ1x1UtrygczKjennA_qs7OWmgOewcuYSYF3Gc_mPbqsDh2YoGCDPL0RxDQ',
+  PUSH_MSG_SUBSCRIBE_URL: 'https://dicoding-movie-push-notif.netlify.app/.netlify/functions/subscribe',
+  PUSH_MSG_UNSUBSCRIBE_URL: 'https://dicoding-movie-push-notif.netlify.app/.netlify/functions/unsubscribe',
 };
 
 export default CONFIG;
--- webpack.common.js
+++ webpack.common.js
@@ -1,9 +1,13 @@
 const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
+require('dotenv').config({
+  path: path.resolve('.env'),
+});
 module.exports = {
   entry: {
     app: path.resolve(__dirname, 'src/scripts/index.js'),
@@ -29,6 +33,9 @@ module.exports = {
     ],
   },
   plugins: [
+    new webpack.DefinePlugin({
+      'process.env': JSON.stringify(process.env),
+    }),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       filename: 'index.html',
